b72379df045e870a8747083d6217fc77d78a83c9
iivvv <734994096@qq.com>
2023-07-09 19:51:33 +0800

    husky lint test

--- src/service/login/login.ts
+++ src/service/login/login.ts
@@ -1,6 +1,6 @@
-import { localCache } from '@/utils/cache'
+// import { localCache } from '@/utils/cache'
 import hyRequest from '..'
-import { LOGIN_TOKEN } from '@/global/constants'
+// import { LOGIN_TOKEN } from '@/global/constants'
 
 export function accountLogin(account: any) {
   return hyRequest.post({
